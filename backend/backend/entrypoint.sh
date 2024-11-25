11ec86482df9a4b2bb678ace7c7a3eb2fc647c2e
Faizullin <osmanfajzulla@gmail.com>
2024-11-26 02:00:52 +0500

    save

--- backend/backend/entrypoint.sh
+++ backend/backend/entrypoint.sh
@@ -11,4 +11,9 @@ echo "Running migrations..."
 python3 manage.py makemigrations --settings=backend.settings.local
 echo "Migrations complete"
 
+echo "Running migrate..."
+python3 manage.py migrate --settings=backend.settings.local
+echo "Migrate complete"
+
+
 exec "$@"
